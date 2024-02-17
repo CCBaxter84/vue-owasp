2a110494b8ae4d996e962436f1b9e62779eac819
CCBaxter84 <troyceclark@gmail.com>
2024-02-17 06:56:32 -0700

    Added computed property to Post

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,16 +1,20 @@
 <script setup>
-defineProps({
+import { computed } from "vue"
+const props = defineProps({
   post: {
     type: Object,
     required: true
   }
+})
+const title = computed(() => {
+  return props.post.title.toUpperCase()
 })
 </script>
 
 <template>
   <article  style="margin: 1rem 2rem;"
             class="is-flex is-flex-direction-column">
-    <h2>{{ post.title }}</h2>
-    <p>{{ post.body }}</p>
+    <h2>{{ title }}</h2>
+    <p>{{ props.post.body }}</p>
   </article>
 </template>
