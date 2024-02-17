c2f6348f9ec34b8cc18947e72419bfd5c9df0824
CCBaxter84 <troyceclark@gmail.com>
2024-02-17 12:44:09 -0700

    initial copying of components

---
+++ src/components/better/Post.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { computed } from "vue"
+const props = defineProps({
+  source: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <article  style="margin: 1rem 2rem;"
+            class="is-flex is-flex-direction-column">
+    <h2>{{ props.source.title }}</h2>
+    <p>{{ props.source.body }}</p>
+  </article>
+</template>
---
+++ src/components/better/Posts.vue
@@ -0,0 +1,29 @@
+<script setup>
+import VirtualList from "vue3-virtual-scroll-list"
+import { ref, onMounted, computed } from "vue"
+import DummyPost from "./DummyPost.vue"
+
+const posts = ref([])
+const hasPosts = computed(() => {
+  return !!(posts.value.length)
+})
+
+onMounted(async() => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+  const initialPosts = await res.json()
+  posts.value = initialPosts
+    .map(post => ({ ...post, title: post.title.toUpperCase() }))
+})
+</script>
+
+<template>
+  <section>
+    <template v-if="hasPosts">
+    <VirtualList  :dataSources="posts"
+                  :dataComponent="DummyPost"
+                  :keeps="50"
+                  dataKey="id"/>
+    </template>
+    <p  v-else>Loading ...</p>
+  </section>
+</template>
